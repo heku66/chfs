4fa5c738fbd1bae5444037fa1bd7dd57420c8b8c
heku66 <102395422+heku66@users.noreply.github.com>
2023-11-12 21:07:15 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,21 @@ ARCH=$(uname -m)
 echo "Current architecture: $ARCH"
 
 if [ "$ARCH" = "x86_64" ]; then
-  wget https://example.com/path/to/app-x86_64.zip -O app.zip
+  DOWNLOAD_URL="http://iscute.cn/tar/chfs/3.1/chfs-linux-amd64-3.1.zip"
+elif [ "$ARCH" = "aarch64" ]; then
+  DOWNLOAD_URL="http://iscute.cn/tar/chfs/3.1/chfs-linux-arm64-3.1.zip"
 else
-  wget https://example.com/path/to/app-arm64.zip -O app.zip
+  echo "Unsupported architecture: $ARCH"
+  exit 1
 fi
 
+# 下载压缩文件
+wget "$DOWNLOAD_URL" -O app.zip
+
+# 解压文件并重命名
 unzip app.zip
+mv chfs-* chfs
+
+# 清理工作目录
 rm app.zip
-
-chmod +x app
-
-./app
+chmod +x chfs
